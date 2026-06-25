a807ba0b5de424ba1cd004a35f3f25ebda4fc2b7
dev3 <dev3@gmail.com>
2026-06-25 11:53:23 +0530

    nxcewg

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 
-x = input("Enter the First Number :")
-y = input("Enter your second Number :")
+x = int(input("Enter the First Number :"))
+y = int(input("Enter your second Number :"))
 
 sum = x + y
 
 print(sum)
 
+a = int(input("Enter the First Number :"))
+b = int(input("Enter your second Number :"))
+
+sub = a - b
 
 print(sub)
